45b00a5714d152f795925b0c7b1b863249a70208
Sarthak singh <sarthaksingh2001@gmail.com>
2024-11-04 21:37:10 +0530

    added migration for channel messages

--- app/models/migrations/003_messages_migration.sql
+++ app/models/migrations/003_messages_migration.sql
@@ -11,5 +11,19 @@ CREATE TABLE IF NOT EXISTS direct_messages (
     FOREIGN KEY (to_user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS channel_messages (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    from_user_id UUID NOT NULL,
+    from_username VARCHAR(255) NOT NULL,
+    to_channel_id UUID NOT NULL,
+    content TEXT NOT NULL,
+    is_edited BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    FOREIGN KEY (from_user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (to_channel_id) REFERENCES channels(id) ON DELETE CASCADE
+);
+
 -- down
 DROP TABLE IF EXISTS direct_messages;
+DROP TABLE IF EXISTS channel_messages;
